f8841439ac6241d7305726145157ad23ae92d5fc
sergeabchiyt-cyber <sergeabchiyt@gmail.com>
2026-08-31 15:39:08 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use axum::{routing::get, Router};
 use std::net::SocketAddr;
+use std::sync::Arc;
+use tokio::sync::RwLock;
 
 mod config;
+mod exchange;
+mod risk;
 mod strategy;
 
 async fn dashboard_handler() -> axum::response::Html<&'static str> {
@@ -11,13 +15,14 @@ async fn dashboard_handler() -> axum::response::Html<&'static str> {
 #[tokio::main]
 async fn main() {
     env_logger::init();
+    let cfg = Arc::new(config::Config::from_env());
+
     let app = Router::new()
         .route("/", get(dashboard_handler));
-        // TODO: Add routes for /api/trades, /api/portfolio
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     log::info!("Dashboard listening on http://{}", addr);
-    
+
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
